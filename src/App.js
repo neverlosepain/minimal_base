86187dc61a61ea740c61666cf120bd103eccc4d4
Pain <145584137+neverlosepain@users.noreply.github.com>
2023-10-01 07:35:20 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-// routes
+
 import Router from './routes';
-// theme
+
 import ThemeProvider from './theme';
-// components
+
 import { StyledChart } from './components/chart';
 import ScrollToTop from './components/scroll-to-top';
 
